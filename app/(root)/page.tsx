8740d59ded34126967165aa8dc1a80d5145d3324
sriram N V <sriramnvilvanathan@gmail.com>
2024-11-21 19:29:02 +0530

    added base heading for homepage with global heading class for reuse by other pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,12 @@
 export default function Home() {
   return (
     <>
-    <h1 className="text-2xl underline">HOME</h1>
+    <section className="pink_container">
+      <h1 className="text-center heading">Pitch your startup,<br/> Connect with Enterprenurs</h1>
+      <p className="sub-heading !max-w-3xl">
+        Submit Ideas and get noticed in virtual competitions  
+       </p>
+    </section>
     </>
   );
 }

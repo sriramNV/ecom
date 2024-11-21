5c22e51b6f316c68af0f03fec36c778d275befa4
sriram N V <sriramnvilvanathan@gmail.com>
2024-11-21 19:52:48 +0530

    Completed search query on header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,8 @@
+import SeachForm from "@/components/SeachForm";
 
+export default async function Home({searchParams}: {searchParams: Promise<{query: string}>}){
+  const query = (await searchParams).query;
 
-export default function Home() {
   return (
     <>
     <section className="pink_container">
@@ -8,6 +10,7 @@ export default function Home() {
       <p className="sub-heading !max-w-3xl">
         Submit Ideas and get noticed in virtual competitions  
        </p>
+       <SeachForm query={query}/>
     </section>
     </>
   );
